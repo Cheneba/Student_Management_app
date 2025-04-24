dd5f217e9c075213679241a4400889498fbf43c2
Cheneba <chenebametuge14@gmail.com>
2025-04-24 13:24:53 +0100

    Created the routes for the students and grouped them

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('students', StudentController::class);
+Route::group(['prefix' => "students"], function () {
+    Route::get('/', [StudentController::class, 'index']);
+    Route::post('/', [StudentController::class, 'store']);
+    Route::get('create', [StudentController::class, 'create']);
+    Route::get('{id}', [StudentController::class, 'show']);
+    Route::get('{id}/edit', [StudentController::class, 'edit']);
+    Route::post('{id}/edit', [StudentController::class, 'update']);
+    Route::post('remove/{id}', [StudentController::class, 'delete']);
+});
 
 Route::get('/', function () {
     return view('layout');
