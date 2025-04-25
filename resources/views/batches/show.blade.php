d4454aa3cbb7a9fa2c958eabe99ca3e6023be029
Cheneba <chenebametuge14@gmail.com>
2025-04-25 12:09:07 +0100

    Because a relationship btn course and batch as been established, just displayed the course name where needed

--- resources/views/batches/show.blade.php
+++ resources/views/batches/show.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="card-body">
         <h2 class="card-title">Name : {{ $batch->name }}</h2>
-        <p class="card-text">Course ID : {{ $batch->course_id }}</p>
+        <p class="card-text">Course ID : {{ $batch->course->name  }}</p>
         <p class="card-text">Start Date : {{ $batch->start_date }}</p>
     </div>
 
